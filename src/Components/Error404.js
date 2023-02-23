dcf9378391b2ce1887b9b7fb621b629dbd529723
istiak ahmed <istiakahmed024@gmail.com>
2023-02-23 16:50:12 +0600

    not found done

--- src/Components/Error404.js
+++ src/Components/Error404.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import notFound from '../assets/images/others/not-found.png'
 
 const Error404 = () => {
     return (
-        <div>
-            <h2>This is not found page</h2>
-        </div>
+        <>
+            <div className="not-found mx-auto p-8">
+                <img src={notFound} alt="" />
+            </div>
+        </>
     );
 };
 
