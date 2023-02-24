2a5aab13c12c6b7c570164c1284a5f0c653c2e13
istiak ahmed <istiakahmed024@gmail.com>
2023-02-24 19:11:52 +0600

    initial server setup

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -8,7 +8,7 @@ const Portfolio = () => {
                     <p> <span>Name:</span> Istiak Ahmed</p>
                     <p> <span>Email:</span> istiakahmed024@gmail.com</p>
                     <p> <span>Educational Background:</span> BA Honors in English, Varendra University Rajshahi</p>
-                    <p> <span>Skills:</span> HTML, CSS, JAVASCRIPT, BOOTSTRAP, JQUERY, REACT, MONGODB, FIREBASE</p>
+                    <p> <span>Skills:</span> HTML, CSS, JAVASCRIPT, BOOTSTRAP, JQUERY, REACT, MONGODB ATLAS, FIREBASE</p>
                     <p> <span>Projects Links:</span> <a className='text-green-500' href="https://ranar-client.vercel.app/" target="_blank" rel="noreferrer" >Ranar</a> <a className='text-green-500' href="https://spria.netlify.app" target="_blank" rel="noreferrer" >Spria</a> <a className='text-green-500' href="https://utrun.netlify.app/" target="_blank" rel="noreferrer" >Utran</a></p>
                 </div>
             </div>
